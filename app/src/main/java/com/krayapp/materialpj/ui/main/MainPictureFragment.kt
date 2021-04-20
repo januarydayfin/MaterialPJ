9114ffb8f8928486b6862ed9533901a66348805a
krayman <50882897+krayman@users.noreply.github.com>
2021-04-20 17:55:30 +0700

    Сделано переключение чипсинок

--- app/src/main/java/com/krayapp/materialpj/ui/main/MainPictureFragment.kt
+++ app/src/main/java/com/krayapp/materialpj/ui/main/MainPictureFragment.kt
@@ -1,11 +1,10 @@
 package com.krayapp.materialpj.ui.main
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
@@ -13,11 +12,16 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import coil.api.load
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.chip.Chip
+import com.krayapp.materialpj.MainActivity
 import com.krayapp.materialpj.R
 import com.krayapp.materialpj.viewmodel.MainViewModel
 import com.krayapp.materialpj.viewmodel.PictureData
 import kotlinx.android.synthetic.main.bottom_sheet_layout.*
 import kotlinx.android.synthetic.main.main_fragment.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainPictureFragment : Fragment() {
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
@@ -49,7 +53,22 @@ class MainPictureFragment : Fragment() {
                 data = Uri.parse("https://en.wikipedia.org/wiki/${input_edit_text.text.toString()}")
             })
         }
+        setChipGroupListener()
         setBottomSheetBehavior(bottom_sheet_container)
+        setBottomAppBar(view)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.bottom_bar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.app_bar_fav -> Toast.makeText(context, "Favorite", Toast.LENGTH_SHORT).show()
+            R.id.app_bar_search -> Toast.makeText(context, "Search", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun renderData(data: PictureData) {
@@ -88,4 +107,34 @@ class MainPictureFragment : Fragment() {
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
     }
 
+    private fun setBottomAppBar(view: View) {
+        val context = activity as MainActivity
+        context.setSupportActionBar(view.findViewById(R.id.bottom_app_bar))
+        setHasOptionsMenu(true)
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun setChipGroupListener() {
+        val dateFormat: DateFormat = SimpleDateFormat("yyyy-MM-dd")
+        val cal: Calendar = Calendar.getInstance()
+        var date:String? = null
+        chipGroup.setOnCheckedChangeListener { chipGroup, position ->
+            chipGroup.findViewById<Chip>(position)?.let {
+                if (it.text == "Сегодня"){
+                    date = null
+                }
+                if (it.text == "Вчера"){
+                    cal.add(Calendar.DATE, -1)
+                    date = dateFormat.format(cal.getTime())
+                }
+                if (it.text == "Позавчера"){
+                    cal.add(Calendar.DATE, -2)
+                    date = dateFormat.format(cal.getTime())
+                }
+                viewModel.getLiveData(date).observe(
+                    viewLifecycleOwner,
+                    Observer { renderData(it) })
+            }
+        }
+    }
 }
