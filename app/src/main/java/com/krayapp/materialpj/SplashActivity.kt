576ff9a9d564b402080180a86302c2369aa6aec8
krayman <50882897+krayman@users.noreply.github.com>
2021-05-21 22:21:21 +0700

    Анимация

--- app/src/main/java/com/krayapp/materialpj/SplashActivity.kt
+++ app/src/main/java/com/krayapp/materialpj/SplashActivity.kt
@@ -3,7 +3,9 @@ package com.krayapp.materialpj
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 
 
 class SplashActivity: AppCompatActivity() {
@@ -12,6 +14,8 @@ class SplashActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        image_view.animate().scaleX(1.5f).scaleY(1.5f).setInterpolator(LinearInterpolator()).duration = 3000
 
         handler.postDelayed({
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
